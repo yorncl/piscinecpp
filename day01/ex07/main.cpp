3be51e23af6a8a60ea7112a5ee21de7c8afe8594
Yorn <yornclaudel@gmail.com>
2020-03-28 11:44:24 +0100

    ex07

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <fstream>
 
 int check_input(int argc, char const *argv[])
 {
@@ -19,17 +20,67 @@ int check_input(int argc, char const *argv[])
 	return 0;
 }
 
+
+void	replaceS1forS2(std::string& s, const std::string& s1, const std::string& s2)
+{
+	size_t l1 = s1.size();
+	size_t l2 = s2.size();
+	size_t ptr;
+	size_t startpos = 0;
+	
+	while ((ptr = s.find(s1, startpos)) != std::string::npos)
+	{	
+		s.replace(ptr, l1, s2);
+		startpos = ptr + l2;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	(void)argc;
-	(void)argv;
-	
 	if (check_input(argc, argv))
 	{
 		std::cout << "\e[31;1mWrong input\e[0m" << std::endl;
 		return 1;
 	}
-	std::fstream stream;
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::ifstream fin;
+	std::ofstream fout;
+	std::string line;
+	fin.open(filename, std::ios::in);
+	if (fin.fail())
+	{
+		std::cerr << "\"" << filename << "\" -- Error: " << strerror(errno) << std::endl;
+		return 1;
+	}
+	fout.open(filename + ".replace", std::ios::out | std::ios::trunc);
+	if (fout.fail())
+	{
+		std::cerr << "Output file -- Error: " << strerror(errno) << std::endl;
+		return 1;
+	}	
+	while (1)
+	{
+		try
+		{
+			getline(fin, line);
+			replaceS1forS2(line, s1, s2);
+			fout << line;
+			if (!fin.eof())
+				fout << std::endl;
+			else
+				break;
+			
+		}
+		catch (const std::exception &e)
+		{
+			std::cerr << "\e[31;1m" <<e.what() << "\e[0m" << '\n';
+			break;
+		}
+	}
+	fin.close();
+	fout.close();
 	std::cout << "Hello world !" << std::endl;
 	return 0;
 }
