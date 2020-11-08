cbcf0aa5134642f4942a3296eb599d1c1587d613
yorn <yornclaudel@gmail.com>
2020-11-08 21:27:26 +0100

    polished ex07 day01

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -4,24 +4,34 @@
 #include <cstring>
 #include <fstream>
 
-int check_input(int argc, char const *argv[])
+static void printUsage()
 {
+	std::cout << "Usage: replace [filename] [s1] [s2]" << std::endl;
+	std::cout << "Replace every occurence of s1 in a file with s2" << std::endl;
+}
 
+static int wrongArgs(int argc, char const *argv[])
+{
 	if (argc != 4)
 	{
-		std::cout << "Number of arguments invalid" << std::endl;
-		return -1;
+		std::cout << "Wrong arguments count" << std::endl;
+		return 1;
 	}
-	if (std::strlen(argv[1]) == 0 || std::strlen(argv[2]) == 0 || std::strlen(argv[3]) == 0)
+	if (std::strlen(argv[1]) == 0)
 	{
-		std::cout << "Arguments cannot be empty" << std::endl;
-		return -1;
+		std::cout << "Filename should not be empty" << std::endl;
+		return 1;
+	}
+	if (std::strlen(argv[2]) == 0)
+	{
+		std::cout << "String to replace should not be empty" << std::endl;
+		return 1;
 	}
 	return 0;
 }
 
 
-void	replaceS1forS2(std::string& s, const std::string& s1, const std::string& s2)
+static void	replaceS1forS2(std::string& s, const std::string& s1, const std::string& s2)
 {
 	size_t l1 = s1.size();
 	size_t l2 = s2.size();
@@ -37,41 +47,46 @@ void	replaceS1forS2(std::string& s, const std::string& s1, const std::string& s2
 
 int main(int argc, char const *argv[])
 {
-	if (check_input(argc, argv))
+	// Arguments check
+	if (wrongArgs(argc, argv))
 	{
-		std::cout << "\e[31;1mWrong input\e[0m" << std::endl;
+		std::cout << std::endl;
+		printUsage();
 		return 1;
 	}
+
+	// variables
 	std::string filename = argv[1];
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 	std::ifstream fin;
 	std::ofstream fout;
 	std::string line;
+
+	// Opening filestreams
 	fin.open(filename, std::ios::in);
 	if (fin.fail())
 	{
-		std::cerr << "\"" << filename << "\" -- Error: " << strerror(errno) << std::endl;
+		std::cerr << " Can't open " << filename << " -- Error: " << strerror(errno) << std::endl;
 		return 1;
 	}
 	fout.open(filename + ".replace", std::ios::out | std::ios::trunc);
 	if (fout.fail())
 	{
-		std::cerr << "Output file -- Error: " << strerror(errno) << std::endl;
+		std::cerr << "Can't open output file (" << filename << ".replace) -- Error: " << strerror(errno) << std::endl;
 		return 1;
-	}	
+	}
+	// Main loop
 	while (1)
 	{
 		try
 		{
-			getline(fin, line);
+			std::getline(fin, line);
 			replaceS1forS2(line, s1, s2);
 			fout << line;
-			if (!fin.eof())
 				fout << std::endl;
-			else
+			if (fin.eof())
 				break;
-			
 		}
 		catch (const std::exception &e)
 		{
@@ -81,6 +96,5 @@ int main(int argc, char const *argv[])
 	}
 	fin.close();
 	fout.close();
-	std::cout << "Hello world !" << std::endl;
 	return 0;
 }
