f08f1f4dac6ae7fb4785beaa6788085eaa6eb184
yorn <yornclaudel@gmail.com>
2020-11-11 01:34:42 +0100

    fixed ex04

--- day01/ex04/ex04.cpp
+++ day01/ex04/ex04.cpp
@@ -6,7 +6,6 @@ int main(void)
 	std::string str = "HI THIS IS BRAIN";
 	std::string* ptr = &str;
 	std::string& ref = str;
-	std::cout << str << std::endl;
 	std::cout << *ptr << std::endl;
 	std::cout << ref << std::endl;
 	return 0;
