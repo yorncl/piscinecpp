0a5ea007c44b28227a9b88245298964be2de776a
yorn <yornclaudel@gmail.com>
2020-11-13 13:17:02 +0100

    fixed ex04

--- day03/ex04/SuperTrap.cpp
+++ day03/ex04/SuperTrap.cpp
@@ -13,6 +13,33 @@ SuperTrap::SuperTrap(std::string n) : ClapTrap(n), FragTrap(n), NinjaTrap(n)
 	std::cout << "SUPER-TP " << _name << " is born!" << std::endl;
 }
 
+SuperTrap::SuperTrap(const SuperTrap &f) : ClapTrap(f._name), FragTrap(f._name), NinjaTrap(f._name)
+{
+    _hit = f._hit;
+	_maxHit = f._maxHit;
+	_energy = f._energy;
+	_maxEnergy = f._maxEnergy;
+	_level = f._level;
+	_melee = f._melee;
+	_ranged = f._ranged;
+	_armor = f._armor;
+	std::cout << "NINJA-TP " << _name << " is born!" << std::endl;
+}
+
+SuperTrap& SuperTrap::operator=(const SuperTrap &f)
+{
+	_hit = f._hit;
+	_maxHit = f._maxHit;
+	_energy = f._energy;
+	_maxEnergy = f._maxEnergy;
+	_level = f._level;
+	_name = f._name;
+	_melee = f._melee;
+	_ranged = f._ranged;
+	_armor = f._armor;
+	return *this;
+}
+
 SuperTrap::~SuperTrap()
 {
 	std::cout << "SUPER-TP " << _name << " is desructed!" << std::endl;
