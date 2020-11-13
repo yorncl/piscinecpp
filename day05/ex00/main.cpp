2c2cfe8f18df29363f76c2f9364f38cdf3e0a95b
yorn <yornclaudel@gmail.com>
2020-11-13 13:39:08 +0100

    fixed exx00 day05

--- day05/ex00/main.cpp
+++ day05/ex00/main.cpp
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+	std::cout << "========== Constructors errors ========" << std::endl;
 	try
 	{
 		Bureaucrat b("John", -52);
@@ -37,6 +38,18 @@ int main(void)
 	{
 		std::cerr << "Error 4 :" << e.what() << '\n';
 	}
+	{
+		std::cout << "========== Copy constructor and assignation ========" << std::endl;
+		Bureaucrat b("alphonse", 56);
+		Bureaucrat b2(b);
+		Bureaucrat b3("test", 22);
+		b3 = b;
+		std::cout << b << std::endl;
+		std::cout << b2 << std::endl;
+		std::cout << b3 << std::endl;
+	}
+
+	std::cout << "========= Simple test =========" << std::endl;
 	try
 	{
 		Bureaucrat b("John", 69);
@@ -51,6 +64,24 @@ int main(void)
 	{
 		std::cerr << "This should not print :" << e.what() << '\n';
 	}
-	
+std::cout << "========= inc dec errortest =========" << std::endl;
+	try
+	{
+		Bureaucrat b("John", 1);
+		b.inc();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "This should print :" << e.what() << '\n';
+	}
+	try
+	{
+		Bureaucrat b("John", 150);
+		b.dec();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "This should print :" << e.what() << '\n';
+	}
 	return 0;
 }
