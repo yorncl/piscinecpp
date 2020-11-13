dc897009ff12ff7b3c2621ee8e8ef7c10593adbf
yorn <yornclaudel@gmail.com>
2020-11-13 12:36:16 +0100

    fixed main scav test duplicata:

--- day03/ex02/main.cpp
+++ day03/ex02/main.cpp
@@ -100,16 +100,15 @@ int main(int ac, char** av)
 		std::srand(std::time(0)); // init random seed
 
 		std::cout << std::endl << "========= Basic constructor =========" << std::endl;
-		ScavTrap *f1 = new ScavTrap("Brigitte");
+		FragTrap *f1 = new FragTrap("Brigitte");
 		(*f1).print();
-		ScavTrap f2("Bob");
+		FragTrap f2("Bob");
 		f2.print();
 		std::cout << std::endl << "========= Copy constructor =========" << std::endl;
-		ScavTrap f3(f2);
+		FragTrap f3(f2);
 		f3.print();
 		std::cout << std::endl << "========= Assignment Operator =========" << std::endl;
-		f1->addEnergy(-10);
-		ScavTrap f4("Diane");
+		FragTrap f4("Diane");
 		f1->meleeAttack("Test");
 		f4 = *f1;
 		f4.print();
@@ -150,11 +149,12 @@ int main(int ac, char** av)
 		f3.rangedAttack("Zer0");
 
 		std::cout << std::endl << "========= Vaulthunter ==========" << std::endl;
-		f4.challengeNewcomer();
-		f4.challengeNewcomer();
-		f4.challengeNewcomer();
-		f4.challengeNewcomer();
-		f4.challengeNewcomer();
+		f4.vaulthunter_dot_exe("Psycho");
+		f4.vaulthunter_dot_exe("Psycho");
+		f4.vaulthunter_dot_exe("Psycho");
+		f4.vaulthunter_dot_exe("Psycho");
+		// SHOULD BE OUT OF ENERGY
+		f4.vaulthunter_dot_exe("Psycho");
 
 		std::cout << std::endl << "========= Mixed tests ==========" << std::endl;
 		f1->takeDamage(5);
@@ -165,7 +165,12 @@ int main(int ac, char** av)
 		f1->beRepaired(50);
 		f1->meleeAttack("le capitalisme");
 		f1->rangedAttack("le capitalisme");
+		f1->vaulthunter_dot_exe("un déferlement de rageux");
+		f1->vaulthunter_dot_exe("un déferlement de rageux");
+		f1->vaulthunter_dot_exe("un déferlement de rageux");
+		f1->vaulthunter_dot_exe("un déferlement de rageux");
 		f1->takeDamage(150);
+		f1->vaulthunter_dot_exe("un déferlement de rageux");
 		f1->meleeAttack("le gang des requins");
 		f1->rangedAttack("le gang des requins");
 		delete f1;
