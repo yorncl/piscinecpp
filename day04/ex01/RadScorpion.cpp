febf701da60d979f96b4d2d17b774834b1e286f8
yorn <yornclaudel@gmail.com>
2020-11-21 00:06:31 +0100

    fromatting to coplien form

--- day04/ex01/RadScorpion.cpp
+++ day04/ex01/RadScorpion.cpp
@@ -5,6 +5,17 @@ RadScorpion::RadScorpion() : Enemy(80, "RadScorpion")
 	std::cout << "* click click click *" << std::endl;
 }
 
+RadScorpion::RadScorpion(const RadScorpion &r) : Enemy(r.getHP(), r.getType())
+{
+	std::cout << "* click click click *" << std::endl;
+}
+
+RadScorpion& RadScorpion::operator=(const RadScorpion &r)
+{
+	Enemy::operator=(r);
+	return *this;
+}
+
 RadScorpion::~RadScorpion()
 {
 	std::cout << "* SPROTCH *" << std::endl;
