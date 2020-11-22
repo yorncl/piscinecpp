7daa379e05ad1a2ea104d83be74be9b04de0e9b5
yorn <yornclaudel@gmail.com>
2020-11-23 00:58:35 +0100

    polishing day03, ex02 maj

--- day03/ex02/main.cpp
+++ day03/ex02/main.cpp
@@ -16,7 +16,7 @@ int main(int ac, char** av)
 	
 	if (ac != 2)
 	{
-		std::cout << "Usage: ./a.out [clap|frag|scav]" << std::endl;
+		std::cout << "Usage: ./a.out [frag|scav]" << std::endl;
 		return 0;
 	}
 
@@ -180,76 +180,8 @@ int main(int ac, char** av)
 		f1->rangedAttack("le gang des requins");
 		delete f1;
 	}
-	else if (strcmp(av[1], "clap") == 0)
-	{
-		std::srand(std::time(0)); // init random seed
-
-		std::cout << std::endl << "========= Basic constructor =========" << std::endl;
-		ClapTrap *f1 = new ClapTrap("Brigitte");
-		(*f1).print();
-		ClapTrap f2("Bob");
-		f2.print();
-		std::cout << std::endl << "========= Copy constructor =========" << std::endl;
-		ClapTrap f3(f2);
-		f3.print();
-		std::cout << std::endl << "========= Assignment Operator =========" << std::endl;
-		f1->addEnergy(-10);
-		ClapTrap f4("Diane");
-		f1->meleeAttack("Test");
-		f4 = *f1;
-		f4.print();
-
-
-		std::cout << std::endl << "========= Melee ==========" << std::endl;
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-		// SHOULD BE OUT OF ENERGY
-		f2.meleeAttack("Bandit");
-		f2.meleeAttack("Bandit");
-
-		std::cout << std::endl << "========= Ranged ==========" << std::endl;
-		f3.rangedAttack("Zer0");
-		f3.rangedAttack("Zer0");
-		f3.rangedAttack("Zer0");
-		f3.rangedAttack("Zer0");
-		// SHOULD BE OUT OF ENERGY
-		f3.rangedAttack("Zer0");
-
-		std::cout << std::endl << "========= Take Damage/Be repaired ==========" << std::endl;
-		f3.addEnergy(500);
-		f3.takeDamage(10);
-		f3.rangedAttack("Zer0");
-		f3.takeDamage(1000);
-		// SHOULD NOT BE ABLE TO DO THIS
-		f3.rangedAttack("Zer0");
-		f3.print();
-		f3.beRepaired(500);
-		f3.rangedAttack("Zer0");
-
-		std::cout << std::endl << "========= Mixed tests ==========" << std::endl;
-		f1->takeDamage(5);
-		f1->takeDamage(10);
-		f1->beRepaired(10);
-		f1->takeDamage(150);
-		f1->meleeAttack("le capitalisme");
-		f1->beRepaired(50);
-		f1->meleeAttack("le capitalisme");
-		f1->rangedAttack("le capitalisme");
-		f1->takeDamage(150);
-		f1->meleeAttack("le gang des requins");
-		f1->rangedAttack("le gang des requins");
-		delete f1;
-	}
 	else
-		std::cout << "Usage: ./a.out [clap|frag|scav]" << std::endl;
+		std::cout << "Usage: ./a.out [frag|scav]" << std::endl;
 	
 	return 0;
 }
