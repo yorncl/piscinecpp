7f9ab1f9d584ccf9183784748c007f13c0a721aa
yorn <yornclaudel@gmail.com>
2020-11-23 02:17:28 +0100

    ex00 day07

--- day07/ex00/whatever.hpp
+++ day07/ex00/whatever.hpp
@@ -1,6 +1,27 @@
 #if !defined(WHATEVER_HPP)
 #define WHATEVER_HPP
 
+template<typename T>
+void swap(T &a, T &b)
+{
+    T *p1 = &a;
+    T *p2 = &b;
+    T tmp = a;
+    
+    *p1 = *p2;
+    *p2 = tmp;
+}
 
+template <typename T>
+T min(T &a, T &b)
+{
+    return a < b ? a : b;
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+    return a > b ? a : b;
+}
 
 #endif // WHATEVER_HPP
