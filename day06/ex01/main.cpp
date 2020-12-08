93742397efb0caec916a63bbd795d4f829a067c7
yorn <yornclaudel@gmail.com>
2020-12-08 01:59:10 +0100

    polishing day06

--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -7,33 +7,6 @@
 
 #include "Data.hpp"
 
-// void *serialize(void)
-// {
-//     char *r = new char[8 + sizeof(int) + 8]; // implicit conversion ?
-//     char *s1 = r;
-//     int *pi = reinterpret_cast<int*> (r + 8);
-//     char *s2 = r + 8 + sizeof(int);
-//     static char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-//     for (size_t i = 0; i < 8; i++)
-//         s1[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-//     *pi = rand() + rand();
-//     for (size_t i = 0; i < 8; i++)
-//         s2[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-//     return reinterpret_cast<void*>(r);
-// }
-
-// Data * deserialize(void * raw)
-// {
-//     char *arr = reinterpret_cast<char*>(raw);
-//     Data* data = new Data();   
-
-//     data->s1 = std::string(arr, 8);
-//     data->n = *(reinterpret_cast<int*>(arr + 8));
-//     data->s2 = std::string(arr + 8 + sizeof(int), 8);
-//     return data;
-// }
-
 void *serialize(void)
 {
     Data *data = new Data();
