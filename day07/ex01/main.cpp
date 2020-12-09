d924f94a92b27596681d4beaa1991b8ff9269a53
yorn <yornclaudel@gmail.com>
2020-12-10 00:52:19 +0100

    adding tests

--- day07/ex01/main.cpp
+++ day07/ex01/main.cpp
@@ -10,8 +10,15 @@ static void print(std::string &s)
 
 int main( void )
 {    
-    std::string arr[] = { "Yo", "mec", "bien", "ou", "bien", " ?"};
+    std::string arr1[] = { "Yo", "mec", "bien", "ou", "bien", " ?"};
+    std::string arr2[] = {"Je suis seul"};
+    std::string arr3[] = {};
 
-    ::iter(arr, 6, print);
+    std::cout << "==== arr1 ====" << std::endl;
+    ::iter(arr1, 6, print);
+    std::cout << "==== arr2 ====" << std::endl;
+    ::iter(arr2, 1, print);
+    std::cout << "==== arr3 ====" << std::endl;
+    ::iter(arr3, 0, print);
     return 0;
 }
