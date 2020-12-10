3a348d7059d0bdc5560623437ba093a4a2c853fb
yorn <yornclaudel@gmail.com>
2020-12-10 01:27:39 +0100

    polishing day07

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -6,55 +6,76 @@
 template<typename T>
 class Array
 {
-public:
-    Array()
-    {
-        _size = 0;
-        _arr = NULL;
-    }
-    Array(unsigned int size)
-    {
-        _size = size;
-        _arr = new T[_size];
-        for (size_t i = 0; i < _size; i++)
-            _arr[i] = 0;
-    }
-    Array(const Array &a)
-    {
-        _size = a._size;
-        _arr = new T[_size];
-        for (size_t i = 0; i < _size; i++)
-            _arr[i] = a._arr[i];
-    }
-    Array &operator=(const Array &a)
-    {
-        if (_arr)
-            delete[] _arr;
-        _size = a._size;
-        _arr = new T[_size];
-        for (size_t i = 0; i < _size; i++)
-            _arr[i] = a._arr[i];
-        return *this;
-    }
-    T &operator[](unsigned int i)
-    {
-        if (i >= _size)
-            throw std::out_of_range("index out of range");
-        return _arr[i];
-    }
-    size_t size()
-    {
-        return _size;
-    }
-
-    ~Array()
-    {
-        delete[] _arr;
-    }
-
 private:
     T* _arr;
     size_t _size;
-    
+
+public:
+    Array();
+    Array(unsigned int size);
+    Array(const Array &a);
+    Array &operator=(const Array &a);
+    T &operator[](unsigned int i);
+    size_t size();
+    virtual ~Array();    
 };
+
+template<typename T>
+Array<T>::Array()
+{
+    _size = 0;
+    _arr = NULL;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int size)
+{
+    _size = size;
+    _arr = new T[_size];
+    for (size_t i = 0; i < _size; i++)
+        _arr[i] = 0;
+}
+
+template<typename T>
+Array<T>::Array(const Array<T> &a)
+{
+    _size = a._size;
+    _arr = new T[_size];
+    for (size_t i = 0; i < _size; i++)
+        _arr[i] = a._arr[i];
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(const Array<T> &a)
+{
+    if (_arr)
+        delete[] _arr;
+    _size = a._size;
+    _arr = new T[_size];
+    for (size_t i = 0; i < _size; i++)
+        _arr[i] = a._arr[i];
+    return *this;
+}
+
+template<typename T>
+T& Array<T>::operator[](unsigned int i)
+{
+    if (i >= _size)
+        throw std::out_of_range("index out of range");
+    return _arr[i];
+}
+
+template<typename T>
+size_t Array<T>::size()
+{
+    return _size;
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+    delete[] _arr;
+}
+
+
 #endif // ARRAY_HPP
