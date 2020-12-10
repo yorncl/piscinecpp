9181dcaacd1b447899d6aa5e74333e4bc36d529c
yorn <yornclaudel@gmail.com>
2020-12-10 01:10:25 +0100

    adding tests

--- day07/ex01/main.cpp
+++ day07/ex01/main.cpp
@@ -2,23 +2,37 @@
 #include <string>
 #include "iter.hpp"
 
-
-static void print(std::string &s)
+template<typename T>
+void printT(T &t)
 {
-    std::cout << "Elem: " << s << std::endl;
+    std::cout << "T: " << t << std::endl;
+}
+
+
+static void printS(std::string &s)
+{
+    std::cout << "String: " << s << std::endl;
+}
+
+static void printI(int &i)
+{
+    std::cout << "Int: " << i << std::endl;
 }
 
 int main( void )
 {    
     std::string arr1[] = { "Yo", "mec", "bien", "ou", "bien", " ?"};
-    std::string arr2[] = {"Je suis seul"};
+    int arr2[] = {1, 2, 3, 4, 5};
     std::string arr3[] = {};
 
     std::cout << "==== arr1 ====" << std::endl;
-    ::iter(arr1, 6, print);
+    ::iter(arr1, 6, printS);
+    ::iter(arr1, 6, printT);
     std::cout << "==== arr2 ====" << std::endl;
-    ::iter(arr2, 1, print);
+    ::iter(arr2, 5, printI);
+    ::iter(arr2, 5, printT);
     std::cout << "==== arr3 ====" << std::endl;
-    ::iter(arr3, 0, print);
+    ::iter(arr3, 0, printS);
+    ::iter(arr3, 0, printT);
     return 0;
 }
